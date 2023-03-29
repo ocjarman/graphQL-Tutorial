2b0f4d91b1c4810408943d1bdda7896cbfc1c795
ocjarman <ocjarman@gmail.com>
2023-03-29 09:58:31 -0400

    initial setup complete

---
+++ schema/type-defs.js
@@ -0,0 +1,16 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  # will hold the queries we want to make inside our api
+  type User {
+    name: String!
+    username: String!
+    age: Int!
+    nationality: String!
+  }
+
+  type Query {
+    # should return a list of users, list nor user can be null
+    users: [User!]!
+  }
+`;
